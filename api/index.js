50ce9acc73c3a829ce9ed46820dbb8d39e0eb31c
Edi <34984294+edihasaj@users.noreply.github.com>
2022-07-08 22:55:53 +0200

    add changes, add missing files

--- api/index.js
+++ api/index.js
@@ -1,10 +1,23 @@
-import express from "express";
-const {
-  Router
-} = express;
-export default (() => {
-  const route = Router();
-  const uploadDir = process.env.UPLOAD_DIR ?? "uploads/images";
-  route.use(`/${uploadDir}`, express.static(uploadDir));
-  return route;
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
 });
+exports["default"] = void 0;
+
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var Router = _express["default"].Router;
+
+var _default = function _default() {
+  var _process$env$UPLOAD_D;
+
+  var route = Router();
+  var uploadDir = (_process$env$UPLOAD_D = process.env.UPLOAD_DIR) !== null && _process$env$UPLOAD_D !== void 0 ? _process$env$UPLOAD_D : "uploads/images";
+  route.use("/".concat(uploadDir), _express["default"]["static"](uploadDir));
+  return route;
+};
+
+exports["default"] = _default;
